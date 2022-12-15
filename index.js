5d9f13ece3ee99725d7277f799090deee74e9f15
CHRISTINA OUBKEO <COUBKEO@GMAIL.COM>
2022-12-14 20:10:58 -0700

    commit tree 0901efe8dd568953aeca7e38e13d9e296320e9db

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++){
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+//}
+//const gifts = ["teddy bear", "drone", "doll"];
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++){
+//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        //debugger;
+//    }
+//    return gifts;
+//}
+//wrapGifts(gifts)
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+     return messages
+    }
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], "birthday"))
+
+function countDown(){
+    for (let countDown = 0; countDown < 11; countDown++) {
+        console.log(countDown)
+    }
+}
